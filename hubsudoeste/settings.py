be4bee1efb821f7c6e9dd279c0cd888d8b3bfaa0
Jeff <jeffersoncerqueira75@gmail.com>
2022-04-07 14:08:59 -0300

    adicionando apps de empresas e contato ao projeto django

--- hubsudoeste/settings.py
+++ hubsudoeste/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'sobre.apps.SobreConfig',
+    'business.apps.BusinessConfig',
+    'contact.apps.ContactConfig',
 ]
 
 MIDDLEWARE = [
--- hubsudoeste/urls.py
+++ hubsudoeste/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
-    path('sobre/', include('sobre.urls'))
+    path('sobre/', include('sobre.urls')),
+    path('empresas/', include('business.urls')),
+    path('contato/', include('contact.urls')),
 ]
